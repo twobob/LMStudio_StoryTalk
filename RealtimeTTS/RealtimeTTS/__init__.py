2935a97a8523756042b10d9888d3a8860aba7d46
_ <simonjohnroberts@msn.com>
2023-12-31 03:03:54 +0000

    Delete RealtimeTTS directory

--- RealtimeTTS/RealtimeTTS/__init__.py
+++
@@ -1,7 +0,0 @@
-from .text_to_stream import TextToAudioStream
-from .engines import BaseEngine
-from .engines import SystemEngine, SystemVoice
-from .engines import AzureEngine, AzureVoice
-from .engines import ElevenlabsEngine, ElevenlabsVoice
-from .engines import CoquiEngine
-from .engines import OpenAIEngine
